a92ce44deaca09a4169ad40220cf7a0e220c494c
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-09-11 10:31:09 +0200

    win / tie logic new / restart game player as a seperate class

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -3,21 +3,21 @@ namespace TicTacToe
     public partial class Form1 : Form
     {
         //aktywny gracz - zaczynaj¹ kó³ka
-        char activePlayer = 'O';
+        Player p = new Player();
 
         public Form1()
         {
             InitializeComponent();
             //zainicjuj labelkê pokazuj¹c¹ aktywnego gracza
-            ActivePlayerLabel.Text = "Aktywny gracz: " + activePlayer;
+            ActivePlayerLabel.Text = "Aktywny gracz: " + p.GetPlayer();
         }
 
         void SwitchPlayer()
         {
             //zmieñ aktywnego gracza
-            activePlayer = (activePlayer == 'O') ? 'X' : 'O';
+            p.SwitchPlayer();
             //zmodyfikuj labelkê pokazuj¹c¹ aktywnego gracza
-            ActivePlayerLabel.Text = "Aktywny gracz: " + activePlayer;
+            ActivePlayerLabel.Text = "Aktywny gracz: " + p.GetPlayer();
         }
 
         private void GameButtonClick(object sender, EventArgs e)
@@ -31,7 +31,7 @@ namespace TicTacToe
                 return;
             }
             //zapisz do guzika aktywnego gracza 
-            button.Text = activePlayer.ToString();
+            button.Text = p.GetPlayer();
             //sprawdzamy czy ktoœ wygra³
             CheckResult();
             //zmiana gracza
@@ -41,6 +41,19 @@ namespace TicTacToe
         }
         void CheckResult()
         {
+            //pobierz wszystkie guziki z layoutu i zapisz do listy
+            List<Button> buttonList = 
+                        tableLayoutPanel1.Controls.OfType<Button>().ToList();
+            //licznik pustych guzików
+            int empty = 0;
+            foreach (Button button in buttonList)
+            {
+                if (button.Text == String.Empty)
+                {
+                    //jeœli guzik nie ma znaku to zwiêksz licznik pustych guzików
+                    empty++;
+                }
+            }
             //sprawdzamy najpierw wiersze
             //sprawdzamy czy ¿aden z guzików w górnym wierszu nie jest pusty
             if(TopLeft.Text != String.Empty && 
@@ -54,6 +67,7 @@ namespace TicTacToe
                     //jeœli s¹ takie same to wygrywa gracz który ma taki sam znak
                     //nie ma znaczenia z którego guzika pobieramy tekst
                     MessageBox.Show("Wygra³ gracz: " + TopLeft.Text);
+                    RestartGame();
                 }
             }
             //sprawdzamy czy ¿aden z guzików w œrodkowym wierszu nie jest pusty
@@ -68,8 +82,33 @@ namespace TicTacToe
                     //jeœli s¹ takie same to wygrywa gracz który ma taki sam znak
                     //nie ma znaczenia z którego guzika pobieramy tekst
                     MessageBox.Show("Wygra³ gracz: " + CenterLeft.Text);
+                    RestartGame();
                 }
             }
+
+            //je¿eli nie wyst¹pi³ ¿aden z warunków wygranej i nie ma pustych guzików
+            //to nast¹pi³ remis
+            if(empty == 0)
+            {
+                MessageBox.Show("Remis");
+                RestartGame();
+            }
+        }
+        void RestartGame()
+        {
+            //pobierz wszystkie guziki z layoutu i zapisz do listy
+            List<Button> buttonList =
+                        tableLayoutPanel1.Controls.OfType<Button>().ToList();
+            //dla ka¿dego guzika w liœcie
+            foreach (Button button in buttonList)
+            {
+                //wyczyœæ tekst
+                button.Text = String.Empty;
+            }
+            //resetuj aktywnego gracza
+            p.Reset();
+            //zmodyfikuj labelkê pokazuj¹c¹ aktywnego gracza
+            ActivePlayerLabel.Text = "Aktywny gracz: " + p.GetPlayer();
         }
     }
 }
---
+++ TicTacToe/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Player
+    {
+        private char activePlayer;
+        public Player()
+        {
+            //aktywny gracz - zaczynają kółka
+            activePlayer = 'O';
+        }
+        public string GetPlayer()
+        {
+            //zwróć symbol gracza jako string do umieszczania na guziku
+            return activePlayer.ToString();
+        }
+        public void SwitchPlayer()
+        {
+            //zmień aktywnego gracza
+            activePlayer = (activePlayer == 'O') ? 'X' : 'O';
+        }
+        public void Reset()
+        {
+            activePlayer = 'O';
+        }
+    }
+}
