7ea93028588cd8ec361e6c74493e3649fa1ff52c
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-09-11 09:24:36 +0200

    check on first row

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -29,132 +29,146 @@
         private void InitializeComponent()
         {
             tableLayoutPanel1 = new TableLayoutPanel();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
+            TopLeft = new Button();
+            TopCenter = new Button();
+            TopRight = new Button();
+            CenterLeft = new Button();
+            CenterCenter = new Button();
+            CenterRight = new Button();
+            BottomLeft = new Button();
+            BottomCenter = new Button();
+            BottomRight = new Button();
+            ActivePlayerLabel = new Label();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.Controls.Add(button9, 2, 2);
-            tableLayoutPanel1.Controls.Add(button8, 1, 2);
-            tableLayoutPanel1.Controls.Add(button7, 0, 2);
-            tableLayoutPanel1.Controls.Add(button6, 2, 1);
-            tableLayoutPanel1.Controls.Add(button5, 1, 1);
-            tableLayoutPanel1.Controls.Add(button4, 0, 1);
-            tableLayoutPanel1.Controls.Add(button3, 2, 0);
-            tableLayoutPanel1.Controls.Add(button2, 1, 0);
-            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33333F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            tableLayoutPanel1.Controls.Add(TopLeft, 0, 1);
+            tableLayoutPanel1.Controls.Add(TopCenter, 1, 1);
+            tableLayoutPanel1.Controls.Add(TopRight, 2, 1);
+            tableLayoutPanel1.Controls.Add(CenterLeft, 0, 2);
+            tableLayoutPanel1.Controls.Add(CenterCenter, 1, 2);
+            tableLayoutPanel1.Controls.Add(CenterRight, 2, 2);
+            tableLayoutPanel1.Controls.Add(BottomLeft, 0, 3);
+            tableLayoutPanel1.Controls.Add(BottomCenter, 1, 3);
+            tableLayoutPanel1.Controls.Add(BottomRight, 2, 3);
+            tableLayoutPanel1.Controls.Add(ActivePlayerLabel, 1, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
             tableLayoutPanel1.Size = new Size(537, 521);
             tableLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // TopLeft
             // 
-            button1.Dock = DockStyle.Fill;
-            button1.Location = new Point(3, 3);
-            button1.Name = "button1";
-            button1.Size = new Size(173, 167);
-            button1.TabIndex = 0;
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += GameButtonClick;
+            TopLeft.Dock = DockStyle.Fill;
+            TopLeft.Location = new Point(3, 43);
+            TopLeft.Name = "TopLeft";
+            TopLeft.Size = new Size(172, 154);
+            TopLeft.TabIndex = 0;
+            TopLeft.UseVisualStyleBackColor = true;
+            TopLeft.Click += GameButtonClick;
             // 
-            // button2
+            // TopCenter
             // 
-            button2.Dock = DockStyle.Fill;
-            button2.Location = new Point(182, 3);
-            button2.Name = "button2";
-            button2.Size = new Size(173, 167);
-            button2.TabIndex = 1;
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += GameButtonClick;
+            TopCenter.Dock = DockStyle.Fill;
+            TopCenter.Location = new Point(181, 43);
+            TopCenter.Name = "TopCenter";
+            TopCenter.Size = new Size(173, 154);
+            TopCenter.TabIndex = 1;
+            TopCenter.UseVisualStyleBackColor = true;
+            TopCenter.Click += GameButtonClick;
             // 
-            // button3
+            // TopRight
             // 
-            button3.Dock = DockStyle.Fill;
-            button3.Location = new Point(361, 3);
-            button3.Name = "button3";
-            button3.Size = new Size(173, 167);
-            button3.TabIndex = 2;
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += GameButtonClick;
+            TopRight.Dock = DockStyle.Fill;
+            TopRight.Location = new Point(360, 43);
+            TopRight.Name = "TopRight";
+            TopRight.Size = new Size(174, 154);
+            TopRight.TabIndex = 2;
+            TopRight.UseVisualStyleBackColor = true;
+            TopRight.Click += GameButtonClick;
             // 
-            // button4
+            // CenterLeft
             // 
-            button4.Dock = DockStyle.Fill;
-            button4.Location = new Point(3, 176);
-            button4.Name = "button4";
-            button4.Size = new Size(173, 167);
-            button4.TabIndex = 3;
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += GameButtonClick;
+            CenterLeft.Dock = DockStyle.Fill;
+            CenterLeft.Location = new Point(3, 203);
+            CenterLeft.Name = "CenterLeft";
+            CenterLeft.Size = new Size(172, 154);
+            CenterLeft.TabIndex = 3;
+            CenterLeft.UseVisualStyleBackColor = true;
+            CenterLeft.Click += GameButtonClick;
             // 
-            // button5
+            // CenterCenter
             // 
-            button5.Dock = DockStyle.Fill;
-            button5.Location = new Point(182, 176);
-            button5.Name = "button5";
-            button5.Size = new Size(173, 167);
-            button5.TabIndex = 4;
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += GameButtonClick;
+            CenterCenter.Dock = DockStyle.Fill;
+            CenterCenter.Location = new Point(181, 203);
+            CenterCenter.Name = "CenterCenter";
+            CenterCenter.Size = new Size(173, 154);
+            CenterCenter.TabIndex = 4;
+            CenterCenter.UseVisualStyleBackColor = true;
+            CenterCenter.Click += GameButtonClick;
             // 
-            // button6
+            // CenterRight
             // 
-            button6.Dock = DockStyle.Fill;
-            button6.Location = new Point(361, 176);
-            button6.Name = "button6";
-            button6.Size = new Size(173, 167);
-            button6.TabIndex = 5;
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += GameButtonClick;
+            CenterRight.Dock = DockStyle.Fill;
+            CenterRight.Location = new Point(360, 203);
+            CenterRight.Name = "CenterRight";
+            CenterRight.Size = new Size(174, 154);
+            CenterRight.TabIndex = 5;
+            CenterRight.UseVisualStyleBackColor = true;
+            CenterRight.Click += GameButtonClick;
             // 
-            // button7
+            // BottomLeft
             // 
-            button7.Dock = DockStyle.Fill;
-            button7.Location = new Point(3, 349);
-            button7.Name = "button7";
-            button7.Size = new Size(173, 169);
-            button7.TabIndex = 6;
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += GameButtonClick;
+            BottomLeft.Dock = DockStyle.Fill;
+            BottomLeft.Location = new Point(3, 363);
+            BottomLeft.Name = "BottomLeft";
+            BottomLeft.Size = new Size(172, 155);
+            BottomLeft.TabIndex = 6;
+            BottomLeft.UseVisualStyleBackColor = true;
+            BottomLeft.Click += GameButtonClick;
             // 
-            // button8
+            // BottomCenter
             // 
-            button8.Dock = DockStyle.Fill;
-            button8.Location = new Point(182, 349);
-            button8.Name = "button8";
-            button8.Size = new Size(173, 169);
-            button8.TabIndex = 7;
-            button8.UseVisualStyleBackColor = true;
-            button8.Click += GameButtonClick;
+            BottomCenter.Dock = DockStyle.Fill;
+            BottomCenter.Location = new Point(181, 363);
+            BottomCenter.Name = "BottomCenter";
+            BottomCenter.Size = new Size(173, 155);
+            BottomCenter.TabIndex = 7;
+            BottomCenter.UseVisualStyleBackColor = true;
+            BottomCenter.Click += GameButtonClick;
             // 
-            // button9
+            // BottomRight
             // 
-            button9.Dock = DockStyle.Fill;
-            button9.Location = new Point(361, 349);
-            button9.Name = "button9";
-            button9.Size = new Size(173, 169);
-            button9.TabIndex = 8;
-            button9.UseVisualStyleBackColor = true;
-            button9.Click += GameButtonClick;
+            BottomRight.Dock = DockStyle.Fill;
+            BottomRight.Location = new Point(360, 363);
+            BottomRight.Name = "BottomRight";
+            BottomRight.Size = new Size(174, 155);
+            BottomRight.TabIndex = 8;
+            BottomRight.UseVisualStyleBackColor = true;
+            BottomRight.Click += GameButtonClick;
+            // 
+            // ActivePlayerLabel
+            // 
+            ActivePlayerLabel.AutoSize = true;
+            ActivePlayerLabel.Dock = DockStyle.Fill;
+            ActivePlayerLabel.Location = new Point(181, 0);
+            ActivePlayerLabel.Name = "ActivePlayerLabel";
+            ActivePlayerLabel.Size = new Size(173, 40);
+            ActivePlayerLabel.TabIndex = 9;
+            ActivePlayerLabel.Text = "Aktywny gracz:";
+            ActivePlayerLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -165,20 +179,22 @@
             Name = "Form1";
             Text = "Form1";
             tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private Button button9;
-        private Button button8;
-        private Button button7;
-        private Button button6;
-        private Button button5;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private Button button1;
+        private Button TopLeft;
+        private Button TopCenter;
+        private Button TopRight;
+        private Button CenterLeft;
+        private Button CenterCenter;
+        private Button CenterRight;
+        private Button BottomLeft;
+        private Button BottomCenter;
+        private Button BottomRight;
+        private Label ActivePlayerLabel;
     }
 }
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -8,12 +8,16 @@ namespace TicTacToe
         public Form1()
         {
             InitializeComponent();
+            //zainicjuj labelkê pokazuj¹c¹ aktywnego gracza
+            ActivePlayerLabel.Text = "Aktywny gracz: " + activePlayer;
         }
 
         void SwitchPlayer()
         {
             //zmieñ aktywnego gracza
             activePlayer = (activePlayer == 'O') ? 'X' : 'O';
+            //zmodyfikuj labelkê pokazuj¹c¹ aktywnego gracza
+            ActivePlayerLabel.Text = "Aktywny gracz: " + activePlayer;
         }
 
         private void GameButtonClick(object sender, EventArgs e)
@@ -31,6 +35,24 @@ namespace TicTacToe
             SwitchPlayer();
             //zablokuj guzik
             //button.Enabled = false;
+        }
+        void CheckResult()
+        {
+            //sprawdzamy najpierw wiersze
+            //sprawdzamy czy ¿aden z guzików w górnym wierszu nie jest pusty
+            if(TopLeft.Text != String.Empty && 
+                TopCenter.Text != String.Empty && 
+                TopRight.Text != String.Empty)
+            {
+                //jeœli nie ma pustych sprawdzamy czy lewy i œrodkowy oraz
+                // œrodkowy i prawy s¹ takie same
+                if(TopLeft.Text == TopCenter.Text && TopCenter.Text == TopRight.Text)
+                {
+                    //jeœli s¹ takie same to wygrywa gracz który ma taki sam znak
+                    //nie ma znaczenia z którego guzika pobieramy tekst
+                    MessageBox.Show("Wygra³ gracz: " + TopLeft.Text);
+                }
+            }
         }
     }
 }
